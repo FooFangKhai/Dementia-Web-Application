931ad4f8e6a6e3bce12eb6c5e21d45d61a719327
FooFangKhai <106471569+FooFangKhai@users.noreply.github.com>
2024-03-09 18:56:29 +0800

    Files

---
+++ myApp/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.base, name="myApp"),
+    path('info/', views.info, name='info'),
+    path('education/', views.education, name='education'),
+    path('events/', views.events, name='events'),
+    path('careevent/', views.careevent, name='careevent'),
+    path('communicationevent/', views.communicationevent, name='communicationevent'),
+    path('images/', views.images, name='image'),
+    path('privacy/', views.privacy, name='privacy'),
+    path('contactpage/', views.contactpage, name='contactpage'),
+    path('organization/', views.organization, name='organization')
+]
